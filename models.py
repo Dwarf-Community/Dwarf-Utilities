3f436fa8d0d50aa474466f33c9089275f2e5cb86
AileenLumina <aileenfromthemoon@gmail.com>
2016-12-15 02:52:30 +0100

    Fixed importing the cog's models.

--- models.py
+++ models.py
@@ -5,3 +5,7 @@ from dwarf.models import User
 class UserInfo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
     utc_offset = models.SmallIntegerField(null=True, blank=True)
+    
+    # every Dwarf Model must define this Meta class
+    class Meta:
+        app_label = 'dwarf'
